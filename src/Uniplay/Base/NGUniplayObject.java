fb7f7de16018fe8b92ac53255e3e57492708e1f8
Nils <nilsgrimmer@gmx.de>
2014-05-12 19:07:27 +0200

    Object Serialization

--- src/Uniplay/Base/NGUniplayObject.java
+++ src/Uniplay/Base/NGUniplayObject.java
@@ -2,7 +2,7 @@ package Uniplay.Base;
 
 import Uniwork.Base.NGObject;
 
-public class NGUniplayObject extends NGObject {
+public abstract class NGUniplayObject extends NGObject {
 
     public NGUniplayObject() {
         super();
