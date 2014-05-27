540625d8b3ced4e7bf9cd0c0494660f3f5fe4980
Nils <nilsgrimmer@gmx.de>
2014-05-27 10:24:33 +0200

    Game control and field stage

--- src/Uniplay/Test/NGTestModule.java
+++ src/Uniplay/Test/NGTestModule.java
@@ -4,6 +4,7 @@ import Uniplay.Kernel.NGGameEngineConstants;
 import Uniplay.Kernel.NGGameEngineMemoryManager;
 import Uniplay.Kernel.NGGameEngineModule;
 import Uniplay.Kernel.NGGameEngineModuleManager;
+import javafx.scene.canvas.Canvas;
 
 public class NGTestModule extends NGGameEngineModule {
 
@@ -12,6 +13,7 @@ public class NGTestModule extends NGGameEngineModule {
         super.AfterInitialize();
         //NGGameEngineMemoryManager manager = (NGGameEngineMemoryManager)ResolveObject(NGGameEngineConstants.CMP_MEMORY_MANAGER, NGGameEngineMemoryManager.class);
         //writeLog(manager.toString());
+        ResolveObject("Canvas0", Canvas.class);
     }
 
     public NGTestModule(NGGameEngineModuleManager aManager, String aName) {
