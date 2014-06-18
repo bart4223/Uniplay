6cfbfd837266d605c7c1f1c674c2239a51f42634
Nils <nilsgrimmer@gmx.de>
2014-06-18 07:45:24 +0200

    Move files

--- src/Uniplay/Test/NGTestModule.java
+++ src/Uniplay/Test/NGTestModule.java
@@ -4,9 +4,36 @@ import Uniplay.Kernel.NGGameEngineConstants;
 import Uniplay.Kernel.NGGameEngineMemoryManager;
 import Uniplay.Kernel.NGGameEngineModule;
 import Uniplay.Kernel.NGGameEngineModuleManager;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 public class NGTestModule extends NGGameEngineModule {
+
+    protected Stage FStage;
+    protected WorkbenchController FController;
+
+    protected void CreateWorkbenchStage(){
+        FStage = new Stage();
+        FXMLLoader lXMLLoader = new FXMLLoader(getClass().getResource("Workbench.fxml"));
+        try {
+            lXMLLoader.load();
+            FController = (WorkbenchController)lXMLLoader.getController();
+            //FGameFieldController.Game = this;
+            Parent lRoot = lXMLLoader.getRoot();
+            FStage.setTitle("Workbench");
+            FStage.setScene(new Scene(lRoot, 500, 500, Color.WHITE));
+            FStage.setResizable(false);
+            FStage.setX(300);
+            FStage.setY(300);
+        }
+        catch( Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void AfterInitialize() {
@@ -15,6 +42,8 @@ public class NGTestModule extends NGGameEngineModule {
         //writeLog(manager.toString());
         //Object obj = ResolveObject("GameFieldController.Layer0", Canvas.class);
         //writeLog(obj.toString());
+        CreateWorkbenchStage();
+        FStage.show();
     }
 
     public NGTestModule(NGGameEngineModuleManager aManager, String aName) {
---
+++ src/Uniplay/Test/WorkbenchController.java
@@ -0,0 +1,4 @@
+package Uniplay.Test;
+
+public class WorkbenchController {
+}
