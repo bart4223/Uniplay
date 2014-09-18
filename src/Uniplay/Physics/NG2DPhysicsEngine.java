ec2e4eaf7e09b59706e63930cd92dbc3cbe048cd
Nils <nilsgrimmer@gmx.de>
2014-09-18 23:35:18 +0200

    First commit for physics engine

---
+++ src/Uniplay/Physics/NG2DPhysicsEngine.java
@@ -0,0 +1,11 @@
+package Uniplay.Physics;
+
+import Uniplay.Kernel.NGGameEngineModuleManager;
+
+public class NG2DPhysicsEngine extends NGPhysicsEngine {
+
+    public NG2DPhysicsEngine(NGGameEngineModuleManager aManager, String aName) {
+        super(aManager, aName);
+    }
+
+}
---
+++ src/Uniplay/Physics/NGPhysicsEngine.java
@@ -0,0 +1,12 @@
+package Uniplay.Physics;
+
+import Uniplay.Kernel.NGGameEngineModule;
+import Uniplay.Kernel.NGGameEngineModuleManager;
+
+public abstract class NGPhysicsEngine extends NGGameEngineModule {
+
+    public NGPhysicsEngine(NGGameEngineModuleManager aManager, String aName) {
+        super(aManager, aName);
+    }
+
+}
